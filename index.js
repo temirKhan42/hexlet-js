36b12ffe7770d5058d4e261042805f2d82df5545
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-06-02 14:19:41 +0600

    Mod file: index.js, part2

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import _ from 'lodash';
 
+<<<<<<< HEAD
 console.log(_.last(['one', 'two']));
+=======
+console.log('Hello, Hexlet!');
+
+console.log(_.last(['one', 'two', 'three']));
+>>>>>>> Mod file: index.js, part2
